4c5fc3eb5995f2f4d28bf8ff1a0457c0fef3042f
Anand Thakur <anandsingh23112005@gmail.com>
2025-08-13 17:42:25 +0530

    some change

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,11 +1,21 @@
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "@/app/_trpc/Client";
+import { httpBatchLink } from "@trpc/client";
 
+const Provider = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [httpBatchLink({ url: "http://localhost:3000/api/trpc" })],
+    })
+  );
 
-const Provider = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+};
 
-export default Provider
+export default Provider;
