13b0c5b51f4811f5cf775259dfd0f6dd1d056c94
Anand Thakur <anandsingh23112005@gmail.com>
2025-09-05 15:40:45 +0530

    some change

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,21 +1,20 @@
 "use client"
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/Client";
 
 const Authcallback = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-
   const origin = searchParams.get("origin");
 
-  const { data ,isLoading  } = trpc.authCallback.useQuery(undefined, {
-    onSuccess: () => {
-      if (data?.success) {
-        router.push(origin ? `/${origin}` : "/dashboard");
-      }
-    },
-  });
+  const { data, isLoading, isSuccess } = trpc.authCallback.useQuery();
+
+  useEffect(() => {
+    if (isSuccess && data?.success) {
+      router.push(origin ? `/${origin}` : "/dashboard");
+    }
+  }, [isSuccess, data, origin, router]);
 
   return <div>{isLoading ? "Loading..." : null}</div>;
 };
